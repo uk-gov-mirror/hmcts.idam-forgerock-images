d6282cd8462fdc17d273bbfabc20203a62ed3d44
Radoslaw Orlowski <radoslaw.orlowski@amido.com>
2020-02-11 14:02:24 +0000

    SIDM-3500 Switch to forgeops release/6.5.2, add images building logic.

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # IDAM ForgeRock Images
 
 The script purpose it to build ForgeRock images for Docker.
+
+## Obtaining ForgeRock Artifactory API Key
+
+To get your API KEY, please follow these instructions:
+https://backstage.forgerock.com/docs/platform/6.5/devops-guide/#devops-implementation-docker-downloader-steps
+
+The key needs to be available to the script as `FR_API_KEY` env variable.
+
+## When to rebuild the images
+
+According to FR documentation:
+
+> A Docker image's contents are static, so if you need to change the content in the image, you must rebuild it. Rebuild images when:
+>
+> - You want to upgrade to newer versions of AM, Amster, IDM, IG or DS software.
+> - You changed files that impact an image's content. Some examples:
+>     - Changes to security files, such as passwords and keystores.
+>     - Changes to file locations or other bootstrap configuration in the AM boot.json file.
+>     - Dockerfile changes to install additional software on the images.
--- build.sh
+++ build.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
-FR_API_KEY="APgwAVvh21GZEng5zSUBgmpMRg8dsW8feBCfL"
+FR_VERSION=6.5.2
 
 # ======================================================================================================================
 # ======================================================================================================================
-function qecho {
+function qecho() {
   [ "$OPTION_QUIET" = "true" ] || echo $@
 }
 
-function title {
+function title() {
   echo -e "==> $1"
 }
 
-function check_dependency {
+function check_dependency() {
   printf -- "- %-15s" "$1"
   [ -x "$(command -v "$1")" ] || { printf -- "not found!\n" && exit 1; }
   printf -- "ok\n"
 }
 
-function check_dependencies {
+function check_dependencies() {
   title "Checking dependencies..."
+  #todo
   check_dependency git
   check_dependency docker
   check_dependency kubectl
@@ -28,17 +29,40 @@ function check_dependencies {
   check_dependency stern
 }
 
-function show_help {
+function require_api_key() {
+  [ -z "$FR_API_KEY" ] && { echo "This operation requires the FR_API_KEY variable!" && exit 1; }
+  qecho "Found FR_API_KEY."
+}
+
+function show_help() {
   printf "Usage: %s [-q] command\n" "$0"
+  echo "Options:"
+  echo -e "\tq\tQuiet"
+  echo
   echo "Where command is one of:"
+  #todo
 }
 
-function initialise {
+function initialise() {
   title "Initialising..."
+  #todo
 }
 
-function build_downloader {
+function build_downloader() {
   title "Building the ForgeRock downloader Docker Image.."
+  require_api_key
+  docker build --no-cache --build-arg API_KEY=$FR_API_KEY --tag forgerock/downloader -f forgeops/docker/downloader/Dockerfile forgeops/docker/downloader || exit 1
+}
+
+function build_fr_all() {
+  title "Building all the ForgeRock Docker Images..."
+
+  # AM
+  docker build --tag forgerock/openam:$FR_VERSION -f forgeops/docker/openam/Dockerfile forgeops/docker/openam || exit 1
+  # IDM
+  docker build --tag forgerock/openidm:$FR_VERSION -f forgeops/docker/openidm/Dockerfile forgeops/docker/openidm || exit 1
+  # DS
+  docker build --tag forgerock/ds:$FR_VERSION -f forgeops/docker/ds/Dockerfile forgeops/docker/ds || exit 1
 }
 
 # ======================================================================================================================
@@ -64,11 +88,19 @@ shift $((OPTIND - 1))
 
 # extract the COMMAND
 case "$1" in
+"")
+  echo "No command provided!"
+  show_help
+  exit 1
+  ;;
 init)
   initialise
   ;;
 build-downloader)
   build_downloader
+  ;;
+build-fr-all)
+  build_fr_all
   ;;
 *)
   echo "Unknown command \"$1\"!"
